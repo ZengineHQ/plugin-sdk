fdba5b5f3c64f9ab72526d012bc0adf247788ca9
Alex Weber <alexweber@mailfence.com>
2020-03-16 14:56:47 -0300

    chore: add packages to index

--- packages/zengine-ui-react/src/index.ts
+++ packages/zengine-ui-react/src/index.ts
@@ -1,12 +1,30 @@
 // Atoms.
 export { default as Button } from './atoms/Button';
 export { default as Checkbox } from './atoms/Checkbox';
+export { default as Image } from './atoms/Image';
+export { default as Input } from './atoms/Input';
+export { default as Label } from './atoms/Label';
+export { default as PageTitle } from './atoms/PageTitle';
+export { default as Radio } from './atoms/Radio';
+export { default as SectionHeader } from './atoms/SectionHeader';
+export { default as Select } from './atoms/Select';
+export { default as Textarea } from './atoms/Textarea';
 
 // Molecules.
-
+export { default as CheckboxField } from './molecules/CheckboxField';
+export { default as CheckboxGroupField } from './molecules/CheckboxGroupField';
+export { default as CounterLabeled } from './molecules/CounterLabeled';
+export { default as NumberField } from './molecules/NumberField';
+export { default as RadioGroupField } from './molecules/RadioGroupField';
+export { default as SelectField } from './molecules/SelectField';
+export { default as TextAreaField } from './molecules/TextAreaField';
+export { default as TextField } from './molecules/TextField';
 
 // Organisms.
-
+export { default as KPI } from './organisms/KPI';
+// export { default as SlideOut } from './organisms/SlideOut';
+export { default as Table } from './organisms/Table';
+export { default as Widget } from './organisms/Widget';
 
 // Utility.
 export { default as Form } from './util/Form';
