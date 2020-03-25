89bbd82d1d81421448d8a055e5db94666298505b
tehpsalmist <bstewardcodes@gmail.com>
2020-03-25 00:09:55 -0400

    chore: remove comments

--- packages/zengine-plugin-docs/src/packages/zengine-ui-react/atoms/Button.stories.js
+++ packages/zengine-plugin-docs/src/packages/zengine-ui-react/atoms/Button.stories.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { boolean, select, text } from '@storybook/addon-knobs';
 
-// import Button from '@zenginehq/zengine-ui-react/lib/atoms/Button';
-// import Button from '@zenginehq/zengine-ui-react/src/atoms/Button';
 import Button from '@zenginehq/zengine-ui-react/lib/atoms/Button';
 import useDefaultPanel from '../../../util/useDefaultPanel';
 
