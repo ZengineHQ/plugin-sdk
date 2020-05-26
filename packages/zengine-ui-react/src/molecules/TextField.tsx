cf4c473f66d5aa85804d6b5e92b5d47d62eb68ef
Alex Weber <alexweber@mailfence.com>
2020-05-26 13:27:13 -0300

    chore: validation progress

--- packages/zengine-ui-react/src/molecules/TextField.tsx
+++ packages/zengine-ui-react/src/molecules/TextField.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { useField } from 'formik';
+import React from "react";
+import { useField } from "formik";
 
-import Input from '../atoms/Input';
-import Label from '../atoms/Label';
-import withForwardRef from '../util/withForwardRef';
-import getFieldClasses from '../util/getFieldClasses';
-import { useFieldValidation, isEmpty } from '../util/validation';
-import ErrorMessage from '../util/ErrorMessage';
+import Input from "../atoms/Input";
+import Label from "../atoms/Label";
+import withForwardRef from "../util/withForwardRef";
+import getFieldClasses from "../util/getFieldClasses";
+import { useFieldValidation, isEmpty } from "../util/validation";
+import ErrorMessage from "../util/ErrorMessage";
 
 export interface TextFieldProps {
-  required?: boolean
-  validate?: Function
-  onChange?: (event: React.ChangeEvent) => void
-  onBlur?: (event: React.FocusEvent) => void
-  id?: string
-  name: string
-  help?: string
-  disabled?: boolean
-  readonly?: boolean
-  placeholder?: string
-  classes?: string
-  innerRef?: any
-  label?: string
-  labelClasses?: string
-  prefix?: string
-  suffix?: string
-  type?: string
+  required?: boolean;
+  validate?: Function;
+  onChange?: (event: React.ChangeEvent) => void;
+  onBlur?: (event: React.FocusEvent) => void;
+  id?: string;
+  name: string;
+  help?: string;
+  disabled?: boolean;
+  readonly?: boolean;
+  placeholder?: string;
+  classes?: string;
+  innerRef?: any;
+  label?: string;
+  labelClasses?: string;
+  prefix?: string;
+  suffix?: string;
+  type?: string;
 }
 
 /**
@@ -35,8 +35,8 @@ export interface TextFieldProps {
  *
  * Use it to collect short textual data from users.
  */
-const TextField: React.FC<TextFieldProps> = props => {
-  const validate = useFieldValidation(props.required, props.validate);
+const TextField: React.FC<TextFieldProps> = (props) => {
+  const validate = useFieldValidation(props.required as boolean, props.validate);
   const [field, meta] = useField({ name: props.name, validate });
   const id = props.id ?? `text-${props.name}`;
   const helpId = !isEmpty(props.help) && !isEmpty(id) ? `${id}-help` : undefined;
@@ -54,56 +54,68 @@ const TextField: React.FC<TextFieldProps> = props => {
 
   const input = (
     <Input
-      type={ props.type }
-      id={ id }
-      disabled={ props.disabled }
-      required={ props.required }
-      placeholder={ props.placeholder }
-      classes={ getFieldClasses(meta, props.classes) }
-      ref={ props.innerRef }
-      describedby={ helpId }
-      { ...field }
+      type={props.type}
+      id={id}
+      disabled={props.disabled}
+      required={props.required}
+      placeholder={props.placeholder}
+      classes={getFieldClasses(meta, props.classes)}
+      ref={props.innerRef}
+      describedby={helpId}
+      {...field}
       // If we have custom onChange and onBlur callbacks we need to wrap them to keep from breaking Formik.
-      onChange={ onChangeHelper }
-      onBlur={ onBlurHelper }
+      onChange={onChangeHelper}
+      onBlur={onBlurHelper}
     />
   );
 
   return (
     <div className="form-group">
-      { !isEmpty(props.label) ? (
-        <Label required={ props.required } for={ id } classes={ props.labelClasses }>{ props.label }</Label>
-      ) : undefined }
+      {!isEmpty(props.label) ? (
+        <Label required={props.required} for={id} classes={props.labelClasses}>
+          {props.label}
+        </Label>
+      ) : undefined}
 
-      { (!isEmpty(props.prefix) || !isEmpty(props.suffix)) ? (
+      {!isEmpty(props.prefix) || !isEmpty(props.suffix) ? (
         <div className="input-group">
-          { !isEmpty(props.prefix) ? (
-            <div className="input-group-append"><span className="input-group-text">{ props.prefix }</span></div>
-          ) : undefined }
+          {!isEmpty(props.prefix) ? (
+            <div className="input-group-append">
+              <span className="input-group-text">{props.prefix}</span>
+            </div>
+          ) : undefined}
 
-          { input }
+          {input}
 
-          { !isEmpty(props.prefix) ? (
-            <div className="input-group-prepend"><span className="input-group-text">{ props.suffix }</span></div>
-          ) : undefined }
+          {!isEmpty(props.prefix) ? (
+            <div className="input-group-prepend">
+              <span className="input-group-text">{props.suffix}</span>
+            </div>
+          ) : undefined}
         </div>
-      ) : input }
+      ) : (
+        input
+      )}
 
-      { !isEmpty(props.help) ? <small id={ helpId } className="form-text text-muted">{ props.help }</small> : undefined }
+      {!isEmpty(props.help) ? (
+        <small id={helpId} className="form-text text-muted">
+          {props.help}
+        </small>
+      ) : undefined}
 
-      <ErrorMessage meta={ meta }/>
+      <ErrorMessage meta={meta} />
     </div>
   );
-}
+};
 
 TextField.defaultProps = {
   disabled: false,
-  label: '',
-  placeholder: '',
+  label: "",
+  placeholder: "",
   required: false,
   readonly: false,
-  classes: '',
-  type: 'text',
+  classes: "",
+  type: "text",
 };
 
 // Exported as a workaround due to Storybook Docs addon not processing wrapped components properly for generated Docs.
--- packages/zengine-ui-react/src/util/validation.ts
+++ packages/zengine-ui-react/src/util/validation.ts
@@ -1,10 +1,11 @@
-import _isObject from 'lodash/isObject';
-import _isEmpty from 'lodash/isEmpty';
+import _isObject from "lodash/isObject";
+import _isEmpty from "lodash/isEmpty";
 
 /**
  * Validates whether a value exists.
  */
-export const exists = (value: any): boolean => value !== null && value !== undefined;
+export const exists = (value: any): boolean =>
+  value !== null && value !== undefined;
 
 /**
  * Validates whether a value is empty.
@@ -16,19 +17,19 @@ export const isEmpty = (value: any): boolean => {
   if (_isObject(value)) {
     return _isEmpty(value);
   }
-  return value === '' || !exists(value);
+  return value === "" || !exists(value);
 };
 
 /**
  * Validates whether a value is a string.
  */
-export const isString = (value: any): boolean => typeof value === 'string';
+export const isString = (value: any): boolean => typeof value === "string";
 
 /**
  * Validates whether a value matches a regex.
  */
 export const matchesRegex = (str: string, regex: RegExp | string): boolean => {
-  const validationRegex = (regex instanceof RegExp ? regex : (new RegExp(regex)));
+  const validationRegex = regex instanceof RegExp ? regex : new RegExp(regex);
   return validationRegex.test(str.toString());
 };
 
@@ -40,20 +41,36 @@ export const isEmail = (string: string): boolean => {
     return false;
   }
   // eslint-disable-next-line
-  return matchesRegex(string, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i);
+  return matchesRegex(
+    string,
+    /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
+  );
 };
 
 /**
  * Factory to return a field validation function.
  */
-export const useFieldValidation = (required: boolean|undefined, customValidation?: Function) => (value:any) => {
-  if (required === true && isEmpty(value)) {
-    return 'Required';
+export const useFieldValidation = (required: Function | boolean, customValidation?: Function) => (value: any) => {
+  // If required is false the field isn't required so just skip this step.
+  if (required !== false) {
+    // If required isn't false but it's not a function, default to a standard empty check.
+    if (typeof required !== "function") {
+      required = isEmpty;
+    }
+
+    // Validate requiredness.
+    if (!required(value)) {
+      return "Required";
+    }
   }
-  if (typeof customValidation === 'function') {
+
+  // Finally, perform additional custom validation.
+  if (typeof customValidation === "function") {
     const res = customValidation(value);
+
+    // Handle rejected promises so they don't throw and instead just trigger an error.
     if (res instanceof Promise) {
-      return res.catch(err => err);
+      return res.catch((err) => err);
     }
     return res;
   }
