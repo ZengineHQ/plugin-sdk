45aeb1d31b3a6490654bba4bcefb83ad867825ff
Alex Weber <alexweber@mailfence.com>
2020-05-26 15:36:03 -0300

    test: test for country field

---
+++ packages/zengine-ui-react/test/molecules/CountryField.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+import { MockForm } from '../MockForm';
+import CountryField from '../../src/molecules/CountryField';
+
+test('it displays a select field with country options', () => {
+  const { container, getByText } = render(<MockForm><CountryField name="foo" /></MockForm>);
+  const firstOption = container.getElementsByTagName('option')[1];
+  const otherOption = container.getElementsByTagName('option')[10];
+
+  expect(firstOption).toHaveAttribute('value', 'AF');
+  expect(otherOption).toHaveAttribute('value', 'AG');
+  expect(getByText('Afghanistan')).toBeInTheDocument();
+  expect(getByText('Brazil')).toBeInTheDocument();
+});
