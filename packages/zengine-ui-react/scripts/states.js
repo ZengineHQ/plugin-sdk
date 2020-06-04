ce71b2dfe9cc374bde06d229df79d56519a54439
Alex Weber <alexweber@mailfence.com>
2020-06-04 14:06:49 -0300

    chore: fetch states as well

--- packages/zengine-ui-react/scripts/states.js
+++ packages/zengine-ui-react/scripts/states.js
@@ -2,12 +2,12 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 
 const CLIENT_ID = require('./client_id').CLIENT_ID;
-const endpoint = `https://api.zenginehq.com/v1/countries?client_id=${ CLIENT_ID }`;
+const endpoint = `https://api.zenginehq.com/v1/states?client_id=${ CLIENT_ID }`;
 
 (async () => {
   const response = await fetch(endpoint);
   const json = await response.json();
   if (json.status === 200) {
-    fs.writeFileSync('./scripts/countries.json', JSON.stringify(json.data));
+    fs.writeFileSync('./scripts/states.json', JSON.stringify(json.data));
   }
 })();
