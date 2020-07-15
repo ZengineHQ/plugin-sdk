61869c522c977a9877911622c592b5c68b0e4da0
Alex Weber <alexweber@mailfence.com>
2020-07-15 06:36:11 -0300

    chore: sort country data

--- packages/zengine-ui-react/src/util/getCountries.ts
+++ packages/zengine-ui-react/src/util/getCountries.ts
@@ -11,7 +11,15 @@ const getCountries = (): SelectOption[] => {
   return data.map((d: CountryData) => ({
     key: d.id,
     value: d.country,
-  }));
+  })).sort((d1: SelectOption, d2: SelectOption) => {
+    if (d1.value < d2.value) {
+      return -1;
+    }
+    if (d1.value > d2.value) {
+      return 1;
+    }
+    return 0;
+  });
 };
 
 export default getCountries;
