e971471b571a302491aa3df15cfe4267addaf79a
Alex Weber <alexweber@mailfence.com>
2020-08-14 14:37:13 -0300

    chore: wip plugin config

--- packages/plugin-config/src/PluginConfig.tsx
+++ packages/plugin-config/src/PluginConfig.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from '@zenginehq/zengine-ui-react/lib';
 
 interface PluginConfigField {
   id: string
   name: string
   help?: string
   type: string // @TODO restrict to available types
+  restrict?: string // @TODO restrict to allowed types
 }
 
 interface PluginConfigPage {
@@ -13,6 +15,10 @@ interface PluginConfigPage {
 }
 
 interface PluginConfigProps {
+  /**
+   * Settings page title
+   */
+  title?: string
   /**
    * Allow multiple plugin configurations
    */
@@ -35,14 +41,38 @@ interface PluginConfigProps {
   pages?: PluginConfigPage[] // @TODO make required
 }
 
+// @TODO figure out how to render icons
+
 const PluginConfig: React.FC<PluginConfigProps> = (props) => {
   console.log('prop', props);
+
+  const createConfig = () => {
+
+  };
+
   return (
-    <h2>hello frontend config</h2>
+    <main className="p-3">
+      <h2>
+        {props.icon && <i className={`icon-${props.icon}`} />}
+        {props.title}
+      </h2>
+
+      {props.multi && <Button theme="success" onClick={createConfig}>Create Configuration</Button>}
+
+      {props.multi ? (<div className="p-2 border mt-2">
+        <p>
+          You don't have any configurations yet.
+          <Button theme="link" onClick={createConfig}>Let's create one!</Button>
+        </p>
+      </div>) : (
+        <div>@TODO display config page directly</div>
+      )}
+    </main>
   );
 }
 
 PluginConfig.defaultProps = {
+  title: 'Plugin Settings',
   multi: false,
   toggle: true,
   help: undefined,
